8c17c89b92a79714730cf51e6f1c0f7975d69378
EmmanuelKakande <kakandemanwell@gmail.com>
2022-12-14 15:46:06 -0800

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,16 @@
 
 int print_last_digit(int n)
 {
+	int a
+
 	if (n < 0)
-	{
 		n = -n;
-	}
 
-	_putchar ('0' + (n % 10));
+	a = n % 10
+	if (a < 0)
+		a = -a;
 
-	return (n % 10);
+	_putchar (a + '0');
+
+	return (a);
 }
