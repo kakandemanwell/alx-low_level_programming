c21c6a37e9a353aaafd1f4df86f1f4fbe356dbb4
EmmanuelKakande <kakandemanwell@gmail.com>
2022-12-14 10:58:48 -0800

    lasts

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digitt in a number
+ * @n: number
+ * Return: digit
+ */
+
+int print_last_digit(int n)
+{
+	int digit = n % 10;
+
+	return (digit);
+
+}
