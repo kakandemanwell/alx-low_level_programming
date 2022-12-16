71f598c1c2370b4d98cda96bfb06fbf8aabece73
EmmanuelKakande <kakandemanwell@gmail.com>
2022-12-16 15:23:06 -0800

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,12 @@ void more_numbers(void)
 	int i;
 
 	for (n = 0; n <= 10; n++)
+	{
 
-		for (i = 0; i <= 14; i++)
+		for (i = 0; i < 15; i++)
 		{
-			if (i / 10 > 0)
-				_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			_putchar(i);
 		}
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
