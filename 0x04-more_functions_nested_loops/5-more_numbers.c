b75f99377dfce02ce9d98517c391156ca15b5819
EmmanuelKakande <kakandemanwell@gmail.com>
2022-12-16 15:07:34 -0800

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints a sequence of numbers 0-14 ten times
+ */
+
+void more_numbers(void)
+{
+	int n;
+	int i;
+
+	for (n = 0; n <= 10; n++)
+
+		for (i = 0; i <= 14; i++)
+			_putchar(i);
+	_putchar('\n');
+}
