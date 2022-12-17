a09475587422e6c21add0484ee7a14f29d9a13d8
EmmanuelKakande <kakandemanwell@gmail.com>
2022-12-16 16:49:40 -0800

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,8 +15,9 @@ void print_square(int size)
 		for (height = 1; height <= size; height++)
 		{
 			for (width = 1; width <= size; width++)
-				_putchar(' ');
-			_putchar('#');
+			{	_putchar(' ');
+				_putchar('#');
+			}
 			_putchar('\n');
 		}
 	}
