46b4370e2a94db72cca46da6eddb504a03fbe3c7
EmmanuelKakande <kakandemanwell@gmail.com>
2022-12-21 17:40:19 -0800

    98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,13 +4,10 @@
  * reset_to_98 - resets the value of a poimter to 98
  *
  * @n: the integer pointer
- * Return: 0 Always
+ * Return: Nothing Always
  */
 
 void reset_to_98(int *n)
 {
-	int p;
-	*n = &p;
-
 	*n = 98;
 }
