965b997f0209e2bb8342b8dc0372f27f03b7aa0d
EmmanuelKakande <kakandemanwell@gmail.com>
2022-12-22 12:09:53 -0800

    halves

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - returns half the input string
+ * @str: the string to halve
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}
