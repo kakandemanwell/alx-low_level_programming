c8eb4626bbfbfba4d7328478f02ed03ef4d9c3f3
EmmanuelKakande <kakandemanwell@gmail.com>
2022-12-21 17:29:14 -0800

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value of a poimter to 98
+ *
+ * @n: the integer pointer
+ * Return: 0 Always
+ */
+
+void reset_to_98(int *n)
+{
+	int p;
+	int *n = &p;
+
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This repo has solutions to pointers and arrays tasks.
