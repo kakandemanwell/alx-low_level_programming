71e636deafcbb2dc6dbfb0b354f803c1dc7cac3b
EmmanuelKakande <kakandemanwell@gmail.com>
2023-01-06 14:32:45 -0800

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - prints the vlue of x to the power y
  * @x: the number to raise to a power y
  * @y: the power to which x is to be raised
- * Return: powered number
+ * Return: powered number and -1 for fsil
  */
 
 int _pow_recursion(int x, int y)
@@ -12,6 +12,10 @@ int _pow_recursion(int x, int y)
 	if (y == 0)
 	{
 		return (1);
+	}
+	else if (y < 0)
+	{
+	return (-1);
 	}
 	return (x * _pow_recursion(x, y - 1));
 }
