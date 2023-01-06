3f20ebc5ef06300884603aa299b0a93d69a581dd
EmmanuelKakande <kakandemanwell@gmail.com>
2023-01-06 08:36:11 -0800

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length of a number
+ * @s: string
+ * Return: the length of s
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (1 + _strlen_recursion(s));
+
+}
