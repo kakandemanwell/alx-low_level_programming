347580f8cbf0108a1d296eb351cbea30847fc67a
EmmanuelKakande <kakandemanwell@gmail.com>
2023-01-10 08:50:47 -0800

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - returns a copy of an input parameter
+ * @str: a string parameter
+ * Return: returns a pointer to a newly allocated copy of str.
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *copy;
+
+	copy  = malloc(sizeof(char) * strlen(str));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+/*copying every caharacter in str iteratively.*/
+	for (i = 0; str[i]; i++)
+	{
+		copy[i] = str[i];
+	}
+	return (copy);
+}
