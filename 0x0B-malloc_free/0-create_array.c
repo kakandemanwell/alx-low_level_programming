71d6544335f6722199435cb45244b8c95ce425d4
EmmanuelKakande <kakandemanwell@gmail.com>
2023-01-10 07:32:34 -0800

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	created_array = malloc(sizeof(char) * size);
 
-	if (size == 0 || craeted_array == NULL)
+	if (size == 0 || created_array == NULL)
 	{
 		return (NULL);
 	}
