d9117ab874f578ae659facdadb6cdeb6135f914d
kakandemanwell <kakandemanwell@gmail.com>
2023-03-26 06:07:17 -0700

    changed link adrress

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp 0x18-dynamic_libraries/giga.so
-export LD_PRELOAD=/tmp/giga.so
+wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so
+export LD_PRELOAD=/tmp/iwin.so
