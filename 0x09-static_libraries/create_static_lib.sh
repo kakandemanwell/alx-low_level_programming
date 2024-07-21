fe3f515019d9f63ad6506b87b29a63ed8933f8d9
kakandemanwell <kakandemanwell@gmail.com>
2024-07-21 14:20:55 -0700

    Changed bash commands

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-#Script to create a static library called liball.a ffrom all .c files in the current directory.
-
-#Compile each .c file to an object file
-for file in *.c
-do
-	gcc -c "$file" -o "${file%.c}.o"
-done
+gcc -c ./*.c
 
 # Create the static library liball.a from the object files
-ar rcs liball.a *.o
-
-# Generating an index for the library.
-ranlib liball.a
-
-# Cleanup the object files.
-rm *.o
+ar -rcs liball.a ./*.o
 
